5039288cd78eea83193d2230aaba6e70881757a4
masody <85034341+masody@users.noreply.github.com>
2021-12-10 23:06:58 +0330

    Add files via upload

---
+++ login form - project/app.js
@@ -0,0 +1,15 @@
+let x = document.getElementById("login");
+let y = document.getElementById("Register");
+let z = document.getElementById("btn");
+
+function Register(){
+    x.style.left = "-450px"
+    y.style.left = "50px"
+    z.style.left = "90px"
+}
+
+function login() {
+    x.style.left = "50px"
+    y.style.left = "450px"
+    z.style.left = "0px" 
+}
